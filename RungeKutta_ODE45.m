2c85829ccce2cee08936f67980030628cc22c4ba
Christopher Steiner <steinch3@zhaw.ch>
2016-09-20 10:46:41 +0200

    4th sample for ode45

--- RungeKutta_ODE45.m
+++ RungeKutta_ODE45.m
@@ -15,4 +15,10 @@ plot(t2, y2);
 % y2' = y1 * t;
 f3 = @(t, y) [y(2)*t; y(1)*t];
 [t3, y3] = ode45(f3, [a, b], [1; 0]);
-plot(t3, y3);
+plot(t3, y3(:,1), t3, y3(:,2)), legend('y1', 'y2');
+
+% y1' = sin(t)*exp(t)-y2
+% y2' = y1 * y2
+f4 = @(t, y) [sin(t)*exp(t)-y(2); y(1) * y(2)];
+[t4, y4] = ode45(f4, [a, b], [1; 0]);
+plot(t4, y4(:,1), t4, y4(:,2)), legend('y1','y2');
