28e841fd1910dd5c15e3b330a6d210638d105b37
Christopher Steiner <steinch3@zhaw.ch>
2016-10-18 10:03:41 +0200

    task 6 completed

--- 5_Clustering/Task6.m
+++ 5_Clustering/Task6.m
@@ -1,9 +1,17 @@
 I = (double(imread('Pics/moon.jpg'))./255);
 I_grey = rgb2gray(I);
-%V = GrayscaleImageToVector(I_grey);
+I_rows = size(I_grey, 1);
+I_columns = size(I_grey, 2);
+V = GrayscaleImageToVector(I_grey);
+alpha = kmeans(V, 2);
+V(alpha == 1, :) = 0;
+V(alpha == 2, :) = 1;
+I_grey = VectorToGrayscaleImage(V, I_rows, I_columns); 
+imagesc(I_grey);
+
 
 % Test of GrayscaleImageToVecotr
-GrayscaleImageToVector([1 2 3; 4 5 6;])
+%GrayscaleImageToVector([1 2 3; 4 5 6;])
 
 % Test of VectorToGrayscaleImage
-VectorToGrayscaleImage([1;4;2;5;3;6],2,3)
+%VectorToGrayscaleImage([1;4;2;5;3;6],2,3)
