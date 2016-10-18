d1b6e3a9d3a3423278fd6b92f3e457faa7b0345f
loetsphi <loetsphi@students.zhaw.ch>
2016-10-18 11:05:41 +0200

    tömes pro tip

--- 5_Clustering/GrayscaleImageToVector.m
+++ 5_Clustering/GrayscaleImageToVector.m
@@ -1,8 +1,7 @@
 function [ V ] = GrayscaleImageToVector(I)
 % I = A grayscale image
 % Constructs a vector by concatenating the clolumns of I
-row_count = size(I, 1);
-column_count = size(I, 2);
+[row_count, column_count] = size(I);
 V = reshape(I, [row_count*column_count, 1]);
 end
 
