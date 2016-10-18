44c9efc911636f453676096e1ff45af26481b440
Christopher Steiner <steinch3@zhaw.ch>
2016-10-18 09:27:38 +0200

    Task 5 Task 2

--- 5_Clustering/WarmupExample.m
+++ 5_Clustering/WarmupExample.m
@@ -1,5 +1,5 @@
 D = [1 2; 12 4; 4 8; 2 3; 5 7; 3 4; 2 2; 6 9; 10 4];
-plot(D(:,1), D(:,2), 'bo');
+%plot(D(:,1), D(:,2), 'bo');
 alpha = kmeans(D,3);
 P1 = D(alpha == 1,:);
 P2 = D(alpha == 2,:);
@@ -11,8 +11,8 @@ hold on;
 % plot(P3(:,1), P3(:,2), 'ro');
 
 %Doppelklick der Files in Datasets um diese zu laden!
-plot_clusters(D, alpha);
+%plot_clusters(D, alpha);
 get_silhouette_value(X1, alpha1)
 get_silhouette_value(X2, alpha2)
 
-cluster_kmeans(X1)
+cluster_kmeans(data10)
--- 5_Clustering/cluster_kmeans.m
+++ 5_Clustering/cluster_kmeans.m
@@ -3,14 +3,15 @@ function cluster_kmeans(data)
 %  k ? {2,3,4,5}, then selects the ’best’ of theses clusterings
 % (with respect to the silhouette value), and finally plots the result
 
+[data_size1, data_size2] = size(data);
 % the silhouette values per cluster size
-silhouette_values = 0;
-alphas = 0;
-for i=1:4
-    alpha(i) = kmeans(data,i+1);
-    silhouete_values(i) = get_silhouette_value(data, alpha);
+silhouette_values = zeros(5, 1);
+alphas = zeros(data_size1, 5);
+for i=2:5
+    alphas(:, i) = kmeans(data,i);
+    silhouete_values(i) = get_silhouette_value(data, alphas(:, i));
 end
 [m, in] = max(silhouete_values);
-plot_clusters(data, alpha(in));
+plot_clusters(data, alphas(:, in));
 end
 
