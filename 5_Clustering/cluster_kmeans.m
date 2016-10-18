feaa1d0ae75a11d143611447184a65ae1cab48fc
loetsphi <loetsphi@students.zhaw.ch>
2016-10-18 09:34:45 +0200

    next gen grammar fix

--- 5_Clustering/cluster_kmeans.m
+++ 5_Clustering/cluster_kmeans.m
@@ -9,9 +9,9 @@ silhouette_values = zeros(5, 1);
 alphas = zeros(data_size1, 5);
 for i=2:5
     alphas(:, i) = kmeans(data,i);
-    silhouete_values(i) = get_silhouette_value(data, alphas(:, i));
+    silhouette_values(i) = get_silhouette_value(data, alphas(:, i));
 end
-[m, in] = max(silhouete_values);
+[m, in] = max(silhouette_values);
 plot_clusters(data, alphas(:, in));
 end
 
