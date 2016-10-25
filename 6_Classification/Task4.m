9b2dd1c851b4b0699f6d441c2e3df7dbab5173d3
Christopher Steiner <steinch3@zhaw.ch>
2016-10-25 11:24:36 +0200

    Task 4

---
+++ 6_Classification/Task4.m
@@ -0,0 +1,15 @@
+%Task 4
+nonskin = zeros(10000,1);
+skin = ones(10000, 1);
+
+I = double(imread('Pics/test.png'))./255;
+[x, y, ~] = size(I);
+M = rgbImage2Matrix(I);
+
+sv = svmtrain([skindata; nonskindata],[skin;nonskin])
+svc = svmclassify(sv, M);
+
+% An error was encountered during classification.
+% Requested 2190x943200 (7.7GB) array exceeds maximum array size preference. Creation of arrays
+% greater than this limit may take a long time and cause MATLAB to become unresponsive. See array
+% size limit or preference panel for more information.
